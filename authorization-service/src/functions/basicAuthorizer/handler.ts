9d3e26c317d165296ec2d44549cf12be73ff8ac1
Darya Drachuk <darya_drachuk@epam.com>
2023-11-08 00:56:14 +0300

    feat: fix Authorizer

--- authorization-service/src/functions/basicAuthorizer/handler.ts
+++ authorization-service/src/functions/basicAuthorizer/handler.ts
@@ -5,30 +5,39 @@ const basicAuthorizer = async (event, _context, callback) => {
   }
 
   try {
+    console.log('process.env', process.env);
     const encodedCreds = event.authorizationToken.split(' ')[1];
     const buff = Buffer.from(encodedCreds, 'base64');
     const plainCreds = buff.toString('utf-8').split(':');
   
     const [login, password] = plainCreds;
-    const effect = process.env[login] === password ? 'Allow' : 'Deny';
-  
+
+    console.log('info', login, password, process.env[login]);
+
+    if(!password || !login || !process.env[login]) {
+      return callback('Unauthorized');
+    }
+    const passwordEnv = process.env[login];
+
+    const effect = passwordEnv && password && (passwordEnv === password) ? 'Allow' : 'Deny';
     const policyDocument = {
-        Version: '2012-10-17',
-        Statement: [
-            {
-                Action: 'execute-api:Invoke',
-                Effect: effect,
-                Resource: event.methodArn,
-            },
+      Version: '2012-10-17',
+      Statement: [
+          {
+              Action: 'execute-api:Invoke',
+              Effect: effect,
+              Resource: event.methodArn,
+          },
         ],
     };
+    console.log('policyDocument', policyDocument);
 
     return callback(null, {
-        principalId: login,
-        policyDocument,
-    });
+      principalId: login,
+      policyDocument,
+    })
   } catch {
-    return callback('Unauthorized');
+    return callback('Forbidden');
   }  
 };
 
